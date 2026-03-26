1011bfb0635d900eb6d69e8ce0dcafcb601c3dcc
YMoonRiver <82157476@qq.com>
2026-03-26 15:56:51 +0800

    init

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -6,10 +6,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) 
-        ERC20("MyToken", "MTK") 
-        Ownable(msg.sender)
-    {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
     }
 
