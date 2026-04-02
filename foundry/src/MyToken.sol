6e50dc013837fcbeedae67ff81683891218e2573
YMoonRiver <82157476@qq.com>
2026-04-02 11:15:46 +0800

    adjust the directory

---
+++ foundry/src/MyToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// OpenZeppelin ERC20
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    event Minted(address indexed to, uint256 amount);
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
